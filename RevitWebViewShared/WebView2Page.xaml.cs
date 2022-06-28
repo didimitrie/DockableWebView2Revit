7cc1f819a125e697bd913fe6bfce8267112e917b
Dimitrie Stefanescu <didimitrie@gmail.com>
2022-06-28 22:40:27 +0100

    shared project + revit version testing

---
+++ RevitWebViewShared/WebView2Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RevitWebView
+{
+  /// <summary>
+  /// Interaction logic for DockPage.xaml
+  /// </summary>
+  public partial class WebView2Page : Page
+  {
+    public WebView2Page()
+    {
+      InitializeComponent();
+    }
+  }
+}
